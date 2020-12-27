f37e74a7ad0a3c48eaf5d379347e9931e6f06a2e
Amir <47950086+amir-shiati@users.noreply.github.com>
2020-12-27 12:50:43 +0330

    fixed bugs

--- subdomain.sh
+++ subdomain.sh
@@ -5,26 +5,34 @@
 SUBDOMAINNAME=$1
 DOMAINUSER=$2
 DOMAINNAME=$3
+IPADDRESS=$4
 
 mkdir /home/$DOMAINUSER/public_html/$SUBDOMAINNAME
 
-cp ./samples/test.onyxfix.ir.conf /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$DOMAINNAME.conf
+cp ./samples/test.domain.conf /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$DOMAINNAME.conf
 #uncommnet if you have a wildcard ssl certification on your server
-#cp ./samples/test.webgram.app.ssl.conf /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$DOMAINNAME.ssl.conf
+#cp ./samples/test.domain.ssl.conf /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$DOMAINNAME.ssl.conf
 
 sed -i -e 's/!test!/'$SUBDOMAINNAME'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.conf
+sed -i -e 's/!ip!/'$IPADDRESS'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.conf
 sed -i -e 's/!domain!/'$DOMAINNAME'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.conf
+sed -i -e 's/!ip!/'$IPADDRESS'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.conf
 sed -i -e 's/!user!/'$DOMAINUSER'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.conf
+sed -i -e 's/!ip!/'$IPADDRESS'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.conf
 
 #uncommnet if you have a wildcard ssl certification on your server
 #sed -i -e 's/!test!/'$SUBDOMAINNAME'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.ssl.conf
+#sed -i -e 's/!ip!/'$IPADDRESS'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.ssl.conf
 #sed -i -e 's/!domain!/'$DOMAINNAME'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.ssl.conf
+#sed -i -e 's/!ip!/'$IPADDRESS'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.ssl.conf
 #sed -i -e 's/!user!/'$DOMAINUSER'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.ssl.conf
+#sed -i -e 's/!ip!/'$IPADDRESS'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.ssl.conf
 
 /usr/local/apache/bin/apachectl -k restart
 
 cat ./samples/test.zone >> /var/named/$DOMAINNAME.db
 sed -i -e 's/!test!/'$SUBDOMAINNAME'/g' /var/named/$DOMAINNAME.db
+sed -i -e 's/!ip!/'$IPADDRESS'/g' /var/named/$DOMAINNAME.db
 
 /usr/local/apache/bin/apachectl -k restart
 service named restart
