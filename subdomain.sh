fcdc4e0068a2f17cea43b7686c390d2d4a0301ce
Amir <47950086+amir-shiati@users.noreply.github.com>
2020-12-27 12:55:47 +0330

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -10,7 +10,7 @@ IPADDRESS=$4
 mkdir /home/$DOMAINUSER/public_html/$SUBDOMAINNAME
 
 cp ./samples/test.domain.conf /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$DOMAINNAME.conf
-#uncommnet if you have a wildcard ssl certification on your server
+#uncommnet if you have a wildcard ssl certificate on your server
 #cp ./samples/test.domain.ssl.conf /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$DOMAINNAME.ssl.conf
 
 sed -i -e 's/!test!/'$SUBDOMAINNAME'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.conf
@@ -20,7 +20,7 @@ sed -i -e 's/!ip!/'$IPADDRESS'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME
 sed -i -e 's/!user!/'$DOMAINUSER'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.conf
 sed -i -e 's/!ip!/'$IPADDRESS'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.conf
 
-#uncommnet if you have a wildcard ssl certification on your server
+#uncommnet if you have a wildcard ssl certificate on your server
 #sed -i -e 's/!test!/'$SUBDOMAINNAME'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.ssl.conf
 #sed -i -e 's/!ip!/'$IPADDRESS'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.ssl.conf
 #sed -i -e 's/!domain!/'$DOMAINNAME'/g' /usr/local/apache/conf.d/vhosts/$SUBDOMAINNAME.$$DOMAINNAME.ssl.conf
